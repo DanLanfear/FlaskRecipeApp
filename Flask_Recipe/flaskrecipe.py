7d4a7dcd2a9c66aec2e659e71673425f027d0f78
Dan Lanfear <lanfeadb@udmercy.edu>
2022-05-02 18:55:53 -0400

    Initial commit

---
+++ Flask_Recipe/flaskrecipe.py
@@ -0,0 +1,16 @@
+from smtpd import DebuggingServer
+from flask import Flask 
+app = Flask(__name__)
+
+
+@app.route("/home")
+@app.route("/")
+def home():
+    return "<h1>Home</h1>"
+
+@app.route("/about")
+def about():
+    return "<h1>About Page</h1>"
+
+if __name__ == '__main__':
+    app.run(debug=True)
