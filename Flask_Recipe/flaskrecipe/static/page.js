d711c7504ab8955663bbf5f2fa90692d275e2d66
Daniel Lanfear <lanfeadb@udmercy.edu>
2022-05-15 20:59:14 -0400

    working on tags in jquery WIP

--- Flask_Recipe/flaskrecipe/static/page.js
+++ Flask_Recipe/flaskrecipe/static/page.js
@@ -29,3 +29,35 @@ $(function() {
         }); //End remove row
     });
 });
+
+
+
+$(function() {
+    $("div[data-toggle=fieldset]").each(function() {
+        var $this = $(this);
+            
+            //Add new entry
+        $this.find("button[data-toggle=fieldset-add-row]").click(function() {
+            var target = $($(this).data("target"))
+            console.log(target);
+            var oldentry = target.find("ul[data-toggle=tag]:last");
+            var entry = oldentry.clone(true, true);
+            console.log(entry.find(":input")[0]);
+            var elem_id = entry.find(":input")[0].id;
+            var elem_num = parseInt(elem_id.replace(/.*-(\d{1,4})-.*/m, '$1')) + 1;
+            entry.attr('data-id', elem_num);
+            entry.find(":input").each(function() {
+                console.log(this);
+                var id = $(this).attr('id').replace('-' + (elem_num - 1) + '-', '-' + (elem_num) + '-');
+                $(this).attr('name', id).attr('id', id).val('').removeAttr("checked");
+            });
+            oldentry.after(entry);
+        }); //End add new entry
+
+        //Remove row
+        $this.find("li[data-toggle=tag-remove]").click(function() {
+            target_remove = $this.find("li[data-toggle=tag-remove]");
+            target_remove.remove();
+        }); //End remove row
+    });
+});
