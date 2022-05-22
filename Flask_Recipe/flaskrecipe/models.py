7e37af4d21ff9db2728626a377e6b2dfa2eb0246
Daniel Lanfear <lanfeadb@udmercy.edu>
2022-05-22 11:32:22 -0400

    added tag to reset.py

--- Flask_Recipe/flaskrecipe/models.py
+++ Flask_Recipe/flaskrecipe/models.py
@@ -60,3 +60,5 @@ class Tag(db.Model):
     recipe_id = db.Column(db.Integer, db.ForeignKey('recipe.id'))
     tag = db.Column(db.String, nullable=False)
 
+    def __repr__(self):
+        return f"Tag('{self.tag}')"
--- Flask_Recipe/reset.py
+++ Flask_Recipe/reset.py
@@ -1,10 +1,13 @@
 from flaskrecipe import db
-from flaskrecipe.models import User, Recipe, Ingredient, Step
+from flaskrecipe.models import Recipe, Ingredient, Step, Tag
 
 Recipe.__table__.drop(db.engine)
 Ingredient.__table__.drop(db.engine)
 Step.__table__.drop(db.engine)
+Tag.__table__.drop(db.engine)
+
 
 Recipe.__table__.create(db.engine)
 Ingredient.__table__.create(db.engine)
 Step.__table__.create(db.engine)
+Tag.__table__.create(db.engine)
